30b9086d1af964c75e0bc3d3f2992263afee1203
oanatopan <19oanatopan@gmail.com>
2026-02-24 07:43:04 +0200

    API- more examples

---
+++ src/test/java/tests/ImageBETest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ImageBETest {
+
+    @Test
+    public void testMethod(){
+        RestAssured.baseURI = "https://api.practicesoftwaretesting.com";
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-type", "application/json");
+        request.header("Accept", "application/json");
+
+        //Pasul 1: Accesam toate imaginile
+        System.out.println("STEP 1: GET ALL IMAGES REQUEST");
+        Response response1 = request.get("/images");
+        System.out.println(response1.getStatusLine());
+        response1.body().prettyPrint();
+        Assert.assertEquals(response1.getStatusCode(), 200);
+    }
+}Re
